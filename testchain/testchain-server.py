3eff361d939d3feda9c7a30951475ea52c055f2e
Fischa7 <janosch.fischer89@googlemail.com>
2018-02-27 15:44:29 +0100

    added proof of work and mining functionality

--- testchain/testchain-server.py
+++ testchain/testchain-server.py
@@ -6,13 +6,13 @@ import json
 import requests
 import datetime as date
 
-node = FLASK(__name__)
+node = Flask(__name__)
 
 # Generate genesis block
 def create_genesis_block():
     # Manually construct the genesis block
     return Block(0,date.datetime.now(),{
-        "proof-of-work": 9,
+        "proof-of-work": 7,
         "transactions": None
         }, "0")
 
@@ -41,7 +41,26 @@ def transaction():
     return "Transaction successful\n"
 
 @node.route('/blocks', methods=['GET'])
-        def get_blocks():
+def get_blocks():
+    chain_to_send = blockchain
+    blocklist = ""
+    for i in range(len(chain_to_send)):
+        block = chain_to_send[i]
+        block_index = str(block.index)
+        block_timestamp = str(block.timestamp)
+        block_data = str(block.data)
+        block_hash = block.hash
+        assembled = json.dumps({
+            "index": block_index,
+            "timestamp": block_timestamp,
+            "data": block_data,
+            "hash": block_hash
+            })
+        if blocklist == "":
+            blocklist = assembled
+        else:
+            blocklist += assembled
+    return blocklist
 
 def find_new_chains():
     # get the chains of every other node
@@ -63,5 +82,47 @@ def consensus():
     blockchain = longest_chain
 
 def proof_of_work(last_proof):
+    # simplified proof of work, number needs to be divisible by 7 and the pow of the previous block
+    inc = last_proof + 1
+    while not (inc % 7 == 0 and inc % last_proof == 0):
+        inc += 1
+    # once the number is found, we can return it as pow
+    return inc
 
+@node.route('/mine', methods = ['GET'])
+def mine():
+    last_block = blockchain[len(blockchain) -1]
+    last_proof = last_block.data['proof-of-work']
+    # find the proof of work for current block being mined
+    proof = proof_of_work(last_proof)
+    # once valid pow is found, we reward the miner by adding a transaction
+    this_nodes_transactions.append(
+            {"from": "network", "to": miner_address, "amount": 1}
+            )
+    # now gather data to create new block
+    new_block_data = {
+            "proof-of-work": proof,
+            "transactions": list(this_nodes_transactions)
+            }
+    new_block_index = last_block.index + 1 
+    new_block_timestamp = this_timestamp = date.datetime.now()
+    last_block_hash = last_block.hash
+    # empty transaction list
+    this_nodes_transactions[:] = []
+    # now create the new block
+    mined_block = Block(
+            new_block_index,
+            new_block_timestamp,
+            new_block_data,
+            last_block_hash)
+    blockchain.append(mined_block)
 
+    # let the client know, the block has been mined
+    return json.dumps({
+        "index": new_block_index,
+        "timestamp": str(new_block_timestamp),
+        "data": new_block_data,
+        "hash": last_block_hash
+    })
+
+    node.run()
